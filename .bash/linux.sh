b9b18b16253584535d896f90940cfd3feb4f7910
satzdev <student003@gmail.com>
2015-02-08 17:57:21 +0900

    update license to cc0

--- .bash/linux.sh
+++ .bash/linux.sh
@@ -1,10 +1,4 @@
 #
-# This program is free software. It comes without any warranty, to
-# the extent permitted by applicable law. You can redistribute it
-# and/or modify it under the terms of the Do What You Want To Public
-# License, Version 3, as published by Devin Weaver. See
-# http://tritarget.org/wywtpl/COPYING for more details.
-#
 # OVERVIEW: Linux specific  configuration.
 
 if [[ ! "$OSTYPE" = 'linux-gnu' ]]; then
